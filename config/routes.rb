4f5667c9e4192e4343e79d793c01a7605dc2db25
Felipe <fperezferraro@gmail.com>
2023-11-22 20:12:32 -0300

    Created Api routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
     resources :recipe_foods
   end
   resources :foods, only: [:index, :new, :create, :destroy]
+
+  namespace :api do
+    namespace :v1 do
+      resources :foods
+      resources :users
+    end
+  end
 end
