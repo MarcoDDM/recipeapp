cfd3e2c2fbfb8f62bec8185f55c09e83832ad37b
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-22 13:29:11 -0300

    Crud for Recipe

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :recipes, foreign_key: 'user_id'
-  has_many :foods, foreign_key: 'user_id'
 end
