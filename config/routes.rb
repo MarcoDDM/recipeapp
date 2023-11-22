6962545f9a26021d489a63429ef19111d1a97aaa
Felipe <fperezferraro@gmail.com>
2023-11-22 19:35:19 -0300

    Added routes to recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root 'public_recipes#index'
   devise_for :users
 
   resources :users
+  resources :recipes do
+    resources :shopping_list
+    resources :recipe_foods
+  end
   resources :foods, only: [:index, :new, :create, :destroy]
 end
