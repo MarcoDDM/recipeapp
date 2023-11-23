c84f4586931c34d1de37be42c432a0c6ad64130a
Felipe <fperezferraro@gmail.com>
2023-11-22 21:40:27 -0300

    Added created_at to recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_174603) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_003728) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,12 +36,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_174603) do
   end
 
   create_table "recipes", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.float "preparation_time", null: false
-    t.float "cooking_time", null: false
-    t.text "description", default: "", null: false
-    t.boolean "public", default: false, null: false
+    t.string "name"
+    t.float "preparation_time"
+    t.float "cooking_time"
+    t.text "description"
+    t.boolean "public"
     t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
@@ -59,6 +61,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_174603) do
   end
 
   add_foreign_key "recipe_foods", "foods"
-  add_foreign_key "recipe_foods", "recipes"
-  add_foreign_key "recipes", "users", on_delete: :cascade
+  add_foreign_key "recipes", "users"
 end
