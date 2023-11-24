4904803c7fcc827969f88edb7e129e4f153e7f90
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-24 15:17:17 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,39 @@
 Rails.application.routes.draw do
-  root 'public_recipes#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
+  root to: "foods#index"
+  get 'missing_food', to: 'recipes#missing_food', as: 'missing_food'
 
   resources :users
-  resources :recipes do
-    resources :shopping_list
+  resources :foods
+  resources :recipes do 
     resources :recipe_foods
+    resources :public_togglers 
   end
-  resources :foods, only: [:index, :new, :create, :destroy]
+  get '/public_recipes', to: 'recipes#public_recipes'
+  resources :users do
+        get :general_food_list, on: :member
+      end
+  # get '/recipes/missing_food', to: 'recipes#missing_food', as: 'missing_food'
+  resources :foods
+  resources :recipes
+  resources :recipe_foods, only: [:new, :create, :index]
 
-  namespace :api do
-    namespace :v1 do
-      resources :foods
-      resources :users
-    end
-  end
 end
+ 
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   devise_for :users
+#   root to: "users#index"
+#   get '/recipes/missing_food', to: 'recipes#missing_food', as: 'missing_food'
+#   get '/public_recipes' => 'recipes#public_recipes'
+#   resources :users do
+#     get :general_food_list, on: :member
+#   end
+#   resources :foods
+#   resources :recipes
+#   resources :recipe_foods, only: [:new, :create, :index]
+
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
